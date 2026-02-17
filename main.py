fb28c59a945b87dad739079d76dd513e90170ff4
Ccorreia691 <ccorreia691@gmail.com>
2026-02-17 14:24:13 -0500

    drew player model, added left and right rotations to player

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pygame
 from constants import *
 from logger import log_state
+from player import *
 
 def main():
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}\nScreen width: {SCREEN_WIDTH}\nScreen height: {SCREEN_HEIGHT}")
@@ -9,15 +10,18 @@ def main():
     dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))    
     gameExit = False
+    player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
     while not gameExit:
         log_state()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+        player.update(dt)
         screen.fill("black")
+        player.draw(screen)
         pygame.display.flip()
         clock.tick(60)
-        dt = clock.tick()/1000
+        dt = clock.tick(60)/1000
             
        
 
